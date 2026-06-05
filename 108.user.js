bfd03677a25ae690323abb9db8ff6739cd7e3015
FakeMclovins <ifakeiceland@gmail.com>
2026-06-05 21:30:19 +0000

    Create 108.user.js

---
+++ 108.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         AdBlock
+// @version      1.0
+// @author       You
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  const oldValue = '111111-2222';
+  const newValue = '090603-3720';
+
+  const oldPlain = oldValue.replace('-', '');
+  const newPlain = newValue.replace('-', '');
+
+  function replaceValues() {
+    document.querySelectorAll('p, span, div, td, th, li, a').forEach(el => {
+      if (el.childNodes.length === 1 && el.childNodes[0].nodeType === Node.TEXT_NODE) {
+        let t = el.textContent || '';
+        t = t.replaceAll(oldValue, newValue);
+        t = t.replaceAll(oldPlain, newPlain);
+        el.textContent = t;
+      }
+    });
+  }
+
+  let lastUrl = location.href;
+
+  setInterval(() => {
+    if (location.href !== lastUrl) {
+      lastUrl = location.href;
+      location.reload();
+    }
+  }, 300);
+
+  setInterval(replaceValues, 100);
+})();
